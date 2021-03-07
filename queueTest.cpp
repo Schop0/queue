9a4e325e4fb60f492861b6af179fd303402e8c0c
Peter Buijs <git@peterbuijs.eu>
2021-03-07 23:53:43 +0100

    Empty failing test

---
+++ queueTest.cpp
@@ -0,0 +1,19 @@
+#include "CppUTest/TestHarness.h"
+
+#include "queue.hpp"
+
+TEST_GROUP(queue)
+{
+	void setup()
+	{
+	}
+
+	void teardown()
+	{
+	}
+};
+
+TEST(queue, init)
+{
+	FAIL("CppUTest says Hi!");
+}
