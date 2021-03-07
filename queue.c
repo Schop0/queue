d3f66f844b40cad3403602ea29c0248fca7fd8f0
Peter Buijs <git@peterbuijs.eu>
2021-03-08 00:53:16 +0100

    Single element storage passing test

---
+++ queue.c
@@ -0,0 +1,10 @@
+#include "queue.h"
+
+void q_push(uint8_t value)
+{
+}
+
+uint8_t q_pop(void)
+{
+	return 0x55;
+}
---
+++ queue.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+
+void q_push(uint8_t value);
+uint8_t q_pop(void);
