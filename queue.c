35afd18bcd85389f35633e7e8438d87311306df6
Peter Buijs <git@peterbuijs.eu>
2021-03-08 01:32:24 +0100

    Simultaneous element storage passing test

--- queue.c
+++ queue.c
@@ -1,13 +1,15 @@
 #include "queue.h"
 
-static uint8_t data;
+static uint8_t data[2];
+static uint8_t *head = &data[0];
+static uint8_t *tail = &data[0];
 
 void q_push(uint8_t value)
 {
-	data = value;
+	*head++ = value;
 }
 
 uint8_t q_pop(void)
 {
-	return data;
+	return *tail++;
 }
