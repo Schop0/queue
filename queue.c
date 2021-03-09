67f4b42a7f512b6aa4e1a096085ed7ee004d927c
Peter Buijs <git@peterbuijs.eu>
2021-03-09 22:50:15 +0100

    Refactor queue storage variables into struct

--- queue.c
+++ queue.c
@@ -1,20 +1,20 @@
 #include "queue.h"
 
-static uint8_t data[2];
-static uint8_t *head = &data[0];
-static uint8_t *tail = &data[0];
+uint8_t buff[2];
+queue_t queue = {2, buff, buff};
+queue_t *q = &queue;
 
 void q_push(uint8_t value)
 {
-	*head++ = value;
+	*(q->head)++ = value;
 }
 
 uint8_t q_pop(void)
 {
-	return *tail++;
+	return *(q->tail)++;
 }
 
 size_t q_max(void)
 {
-	return sizeof data;
+	return q->length;
 }
--- queue.h
+++ queue.h
@@ -1,5 +1,14 @@
 #include <stdint.h>
 
+typedef struct {
+	size_t length;
+	uint8_t *data;
+	uint8_t *head;
+	uint8_t *tail;
+} queue_t;
+
+extern queue_t *q;
+
 void q_push(uint8_t value);
 uint8_t q_pop(void);
 size_t q_max(void);
