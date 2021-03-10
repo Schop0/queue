77d20674a68a064bdca44497368a75c522fe1bad
Peter Buijs <git@peterbuijs.eu>
2021-03-10 02:09:20 +0100

    Update tests to provide storage to all q_* calls

--- queueTest.cpp
+++ queueTest.cpp
@@ -2,18 +2,22 @@
 
 #include "queue.hpp"
 #include <cstring>
+#include <iostream>
 
+#define TESTSIZE_SMALL 2
 #define TESTVALUE 0x55
 #define RANDVALUE (rand() % 0xff)
 
 TEST_GROUP(queue)
 {
+	uint8_t buffer[TESTSIZE_SMALL];
+	queue_t queue;
+
 	void setup()
 	{
+		memset(buffer, TESTVALUE, sizeof buffer);
+		memset(&queue, TESTVALUE, sizeof queue );
 		srand(TESTVALUE);
-		q->head = q->buff;
-		q->tail = q->buff;
-		memset(q->buff, TESTVALUE, q->size);
 	}
 
 	void teardown()
@@ -22,8 +26,8 @@ TEST_GROUP(queue)
 
 	void test_push_pop(uint8_t value)
 	{
-		q_push(value);
-		BYTES_EQUAL(value, q_pop());
+		q_push(&queue, value);
+		BYTES_EQUAL(value, q_pop(&queue));
 	}
 
 	void test_push_pop_random_data(size_t length)
@@ -33,42 +37,42 @@ TEST_GROUP(queue)
 
 		for(i=0; i<length; i++) {
 			data[i] = RANDVALUE;
-			q_push(data[i]);
+			q_push(&queue, data[i]);
 		}
 
 		for(i=0; i<length; i++)
-			BYTES_EQUAL(data[i], q_pop());
+			BYTES_EQUAL(data[i], q_pop(&queue));
 	}
 };
 
 TEST(queue, singleElement)
 {
+	CHECK(q_init(&queue, buffer, sizeof buffer));
 	test_push_pop(TESTVALUE);
 }
 
 TEST(queue, twoElementsSequential)
 {
+	CHECK(q_init(&queue, buffer, sizeof buffer));
 	test_push_pop(RANDVALUE);
 	test_push_pop(RANDVALUE);
 }
 
 TEST(queue, twoElementsSimultaneous)
 {
+	CHECK(q_init(&queue, buffer, sizeof buffer));
 	test_push_pop_random_data(2);
 }
 
 TEST(queue, maxElementsSimultaneous)
 {
-	test_push_pop_random_data(q_max());
+	CHECK(q_init(&queue, buffer, sizeof buffer));
+	test_push_pop_random_data(q_max(&queue));
 }
 
 TEST(queue, initQueue)
 {
-	uint8_t buffer[2] = {0};
-	queue_t queue = {0};
-
 	CHECK(q_init(&queue, buffer, sizeof buffer));
-
 	UNSIGNED_LONGS_EQUAL(sizeof buffer, queue.size);
 	POINTERS_EQUAL(buffer, queue.buff);
 	POINTERS_EQUAL(buffer, queue.head);
@@ -77,22 +81,15 @@ TEST(queue, initQueue)
 
 TEST(queue, initVoidQueue)
 {
-	uint8_t buffer[2] = {0};
-
 	CHECK_FALSE(q_init(NULL, buffer, sizeof buffer));
 }
 
 TEST(queue, initVoidData)
 {
-	queue_t queue = {0};
-
-	CHECK_FALSE(q_init(&queue, NULL, 2));
+	CHECK_FALSE(q_init(&queue, NULL, sizeof buffer));
 }
 
 TEST(queue, initZeroSize)
 {
-	uint8_t buffer[2] = {0};
-	queue_t queue = {0};
-
 	CHECK_FALSE(q_init(&queue, buffer, 0));
 }
