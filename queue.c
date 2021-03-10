93543a09de664b8bec2ee3a558fcf96bff881f05
Peter Buijs <git@peterbuijs.eu>
2021-03-10 02:46:04 +0100

    Push, pop and max functions must not dereference NULL passing tests

--- queue.c
+++ queue.c
@@ -15,15 +15,24 @@ bool q_init(queue_t *q, uint8_t *buffer, size_t size)
 
 void q_push(queue_t *q, uint8_t value)
 {
+	if(!q)
+		return;
+
 	*(q->head)++ = value;
 }
 
 uint8_t q_pop(queue_t *q)
 {
+	if(!q)
+		return 0;
+
 	return *(q->tail)++;
 }
 
 size_t q_max(queue_t *q)
 {
+	if(!q)
+		return 0;
+
 	return q->size;
 }
