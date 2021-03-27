a12890676ab20b92236ac6452b00d1c3496d809a
Peter Buijs <git@peterbuijs.eu>
2021-03-27 20:44:43 +0100

    Refactor head and tail pointer increment logic

--- queue.c
+++ queue.c
@@ -1,5 +1,25 @@
 #include "queue.h"
 
+/*
+ * Increment a pointer into a queue, wrapping as needed
+ *
+ * Arguments:
+ *   - The queue associated with the pointer
+ *   - A reference to the pointer to be incremented
+ * Note: arguments are not checked. Ensure that:
+ *   - The queue is valid
+ *   - The pointer target is inside the queue's buffer
+ */
+static void increment_ptr(const queue_t *q, uint8_t **ptr)
+{
+	uint8_t *newPtr = ++(*ptr);
+
+	if(newPtr >= (q->buff + q->size))
+		newPtr = q->buff;
+
+	*ptr = newPtr;
+}
+
 bool q_init(queue_t *q, uint8_t *buffer, size_t size)
 {
 	if(!q || !buffer || !size)
@@ -18,10 +38,8 @@ void q_push(queue_t *q, uint8_t value)
 	if(!q)
 		return;
 
-	if(q->head >= (q->buff + q->size))
-		q->head = q->buff;
-
-	*(q->head)++ = value;
+	*q->head = value;
+	increment_ptr(q, &q->head);
 }
 
 uint8_t q_pop(queue_t *q)
@@ -29,10 +47,10 @@ uint8_t q_pop(queue_t *q)
 	if(!q)
 		return 0;
 
-	if(q->tail >= (q->buff + q->size))
-		q->tail = q->buff;
+	uint8_t value = *q->tail;
+	increment_ptr(q, &q->tail);
 
-	return *(q->tail)++;
+	return value;
 }
 
 size_t q_max(queue_t *q)
