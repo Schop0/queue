48f54da717c2f5d20fcc023b93decc4a11ec4374
Peter Buijs <git@peterbuijs.eu>
2021-04-05 12:57:39 +0200

    Refactor simple getters and setters to inline functions

--- queue.h
+++ queue.h
@@ -13,8 +13,8 @@ typedef struct {
 bool    q_init(queue_t *q, uint8_t *buffer, size_t size);
 bool    q_push(queue_t *q, uint8_t value);
 bool    q_pop (queue_t *q, uint8_t *value);
-bool    q_empty(const queue_t *q);
-bool    q_full(const queue_t *q);
-size_t  q_size(const queue_t *q);
 size_t  q_used(const queue_t *q);
 size_t  q_free(const queue_t *q);
+inline bool   q_empty(const queue_t *q) { return NULL == q ? 0 : q->empty; }
+inline bool   q_full (const queue_t *q) { return NULL == q ? 0 : q->full ; }
+inline size_t q_size (const queue_t *q) { return NULL == q ? 0 : q->size ; }
